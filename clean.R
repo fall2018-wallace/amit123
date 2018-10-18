ad97bb303a31686869ead6cc1c49bfb9a13835fb
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 23:28:30 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,8 +1,7 @@
 
 CleanDataframe <- function() 
 {
-  dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"), stringsAsFactors=FALSE)
-  dfStates <<- dfStates[-1,]
+    dfStates <<- dfStates[-1,]
   dfStates <<- dfStates[-52,]
   dfStates <<- dfStates[-9,]
   dfStates <<- dfStates[,-1:-4]
