ce220e85606a9fdc7057c398f63da1c4bf2ec447
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 12:13:26 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -1,4 +1,2 @@
 
-merged_data_new <- data.frame(merged_data,state.area,state.center)
-US <- map_data("state")
-merged_data_new$stateName <- tolower(merged_data_new$stateName)
+merged_data <- merge(dfStates,arrests,By ="stateName")
